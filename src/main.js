f38f54a7a0a71e0f886ebf74b23743c4289b27af
canbulgay <canbulgay@outlook.com>
2022-02-11 14:20:29 +0300

    Home page entegration done, seperated Bookmarklistitems

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 import "@/assets/style.css";
 import appHeader from "@/components/Shared/appHeader";
+import appBookmarkList from "@/components/Shared/appBookmarkList";
 
-createApp(App).component('appHeader',appHeader).use(store).use(router).mount('#app')
+createApp(App).component('appBookmarkList',appBookmarkList).component('appHeader',appHeader).use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <app-header />
+  <div class="flex flex-row">
+    <Sidebar />
+
+  <app-bookmark-list />
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Sidebar from "@/components/Home/Sidebar"
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Sidebar
   }
 }
 </script>
