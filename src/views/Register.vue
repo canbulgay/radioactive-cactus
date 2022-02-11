513ca48d8405a432978690b10983cf63d1747e56
canbulgay <canbulgay@outlook.com>
2022-02-11 13:58:29 +0300

    Create Register and Login page , set routes in routes/index.js

---
+++ src/views/Register.vue
@@ -0,0 +1,14 @@
+<template>
+        <div class="login_register_container">
+        <h3 class="text-2xl text-center mb-3">Register</h3>
+        <input type="text" placeholder="Full Name" class="input mb-3" />
+        <input type="text" placeholder="Username" class="input mb-3" />
+        <input type="password" placeholder="Password" class="input mb-3" />
+        <button class="default-button">Sign Up</button>
+        <span class="text-center mt-3 text-sm">
+        I am already a member,
+        <a href="#" class="text-red-900 hover:text-black">Sign In!</a>
+        </span>
+    </div>
+</template>
+
